f338ebad8c0d8e3c2f3828b654f7ad6ed37d388d
dave1415 <dawitgetaneh99@gmail.com>
2022-08-30 11:48:51 +0000

    dave

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 from calculator_1 import add, sub, mul, div
+
+
 def main():
-
-
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
