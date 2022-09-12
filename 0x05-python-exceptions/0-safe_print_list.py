e99217a5dba8e88c0da2c0a51138fbe731f48c24
dave1415 <dawitgetaneh99@gmail.com>
2022-09-12 21:42:40 +0000

    dave

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 
+
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+
+def safe_print_division(a, b):
+    quotient = None
+    try:
+        quotient = a / b
+        print("Inside result: {}".format(quotient))
+    except:
+        print("Inside result: {}".format(quotient))
+    finally:
+        return quotient
+
