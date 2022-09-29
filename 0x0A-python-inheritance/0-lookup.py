8d1771b6f3d3720fb84bccd26a375148bef916ba
dave1415 <dawitgetaneh99@gmail.com>
2022-09-28 22:26:06 -0700

    dave

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""Module 0-lookup.
+Finding a list of available attributes and methods of an object.
+"""
+
+
 def lookup(obj):
-    """Return list of obj"""
-    return (dir(obj))
+    """Returns that list of attributes and methods.
+    Args:
+        - obj: object to look into
+    """
+
+    return dir(obj)
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Module 1-my_list.
+Creates a class inheriting from the list class.
+"""
+
+
 class MyList(list):
-    """Type class MyList  with print_sorted function"""
+    """Class MyList inherits from list."""
 
     def print_sorted(self):
-        print(sorted(self))
+        """Prints the list, in ascending sort."""
+
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
