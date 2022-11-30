f08e1a17a8694c139c9db4be38709cd2d0569ccb
dave1415 <dawitgetaneh99@gmail.com>
2022-11-30 05:43:07 -0800

    dave

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2])) === true) {
+const { argv } = require('process');
+if (isNaN(argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + parseInt(process.argv[2]));
+  console.log(`My number: ${parseInt(argv[2])}`);
 }
